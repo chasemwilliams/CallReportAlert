655b78966190c8a244f41b7efbf219b1f9d017ee
Chase Williams <chase.mwilliams@outlook.com>
2016-11-26 10:31:05 -0500

    Creating new class for credentials

--- CallReportAlertConsoleApp/Program.cs
+++ CallReportAlertConsoleApp/Program.cs
@@ -1,18 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Collections;
+using System.Collections.Specialized;
+using System.ComponentModel;
+//using System.Windows.Forms;
+using System.Data;
+using System.IO;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Xml;
+using System.Timers;
+
+using Microsoft.Web.Services2;
+using Microsoft.Web.Services2.Security;
+using Microsoft.Web.Services2.Security.Tokens;
+using FFIECPublicWebServiceSampleClient;
+
 
 namespace CallReportAlertConsoleApp
 {
     class Program
     {
+        //member variables
         static void Main(string[] args)
         {
             Console.WriteLine(DateTime.Now);
             Console.WriteLine("Testing");
-            Console.WriteLine("Dad please text me if you see this");
+
         }
-    }
+
+    }   
 }
